80bfdcad2ff1b437d3bee21df63968ab59999c65
Peter Steiner <peter.steiner.79.ps@gmail.com>
2017-12-02 14:30:45 +0100

    Day 2 solved

--- d-2.py
+++ d-2.py
@@ -1,7 +1,6 @@
 #!/user/bin/env python3 -tt
 
 """
-Task:s
 https://adventofcode.com/2017/day/2
 """
 
@@ -25,18 +24,30 @@ def solve_a():
         sequences = [int(s) for s in row.split()]
         diff = max(sequences) - min(sequences)
         sum = sum + diff
-    print("Sum: " + str(sum))  
+    print("Solution A: " + str(sum))  
 
 def solve_b():
     sum = 0
     rows = readInput().split("\n")
     for row in rows:
-        sequences = [int(s) for s in row.split()]
-   
-    print("Sum: " + str(sum))  
+        sequences = sorted([int(s) for s in row.split()])
+        sum = sum + findEven(0, sequences)
+
+    print("Solution B: " + str(sum))  
+
+def findEven(index, numbers):
+    divider = numbers[index]
+    result_arr = [n for n in numbers if n != divider and n % divider == 0]
+    if result_arr:
+        return result_arr[0] // divider 
+    if index == len(numbers) - 1:
+        return 0
+    return findEven(index+1, numbers)
 
 if __name__ == '__main__':
+
     solve_a()
-    
+    solve_b()
+
     print("Finished executing: " + task)
     sys.exit(1)
