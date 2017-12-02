cc9b571c401bd74d04ae505d969cd0c641435a11
Peter Steiner <peter.steiner.79.ps@gmail.com>
2017-12-02 12:19:44 +0100

    Day 2 part one

--- d-2.py
+++ d-2.py
@@ -3,7 +3,7 @@ import sys
 import os
 
 """
-Task:
+Task:s
 https://adventofcode.com/2017/day/2
 """
 
@@ -14,16 +14,23 @@ infile=task + ".input"
 # Class declarations
 
 def readInput():
-    with open('input\\' + infile) as f:
-        data = f.read()
-#        print("This is the output of [" + infile + "]:\n" + data)
-    f.close()
+    with open('input\\' + infile) as file:
+        data = file.read()
+    file.close()
     return data
 
 def main():
+    sum = 0
+    rows = readInput().split("\n")
+    for row in rows:
+        sequences = [int(s) for s in row.split()]
+        min_ = min(sequences)
+        max_ = max(sequences)
+        diff = max_ - min_
+        #print( str(max_) + "-" + str(min_) + "=" + str(diff))
+        sum = sum + diff
 
-    data = readInput()
-
+    print("Sum: " + str(sum))  
     print("Finished executing: " + task)
     sys.exit(1)
 
