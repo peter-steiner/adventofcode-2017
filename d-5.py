5137b505303ff2caec9884ff77ee9e360f6a05e6
Peter Steiner <peter.steiner.79.ps@gmail.com>
2017-12-05 06:21:46 +0100

    Prepare day 5

---
+++ d-5.py
@@ -0,0 +1,33 @@
+#!/user/bin/env python3 -tt
+
+"""
+https://adventofcode.com/2017/day/5
+"""
+
+import sys
+
+# Global variables
+task="d-5"
+infile=task + ".input"
+
+movement = {"U": 0, "D": 0}
+visited = {}
+
+
+def solve_a():
+    steps = 0
+
+    print("Solution A: ", steps) 
+
+def solve_b():
+    steps = 0
+
+    print("Solution B: ", steps)
+
+if __name__ == '__main__':
+
+    solve_a()
+    # solve_b()
+
+    print("Finished executing: " + task)
+    sys.exit(1)
