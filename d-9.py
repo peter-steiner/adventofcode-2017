fc601ffc7bae96571e70b2664428b0766a72035d
Peter Steiner <peter.steiner.79.ps@gmail.com>
2017-12-09 09:46:50 +0100

    Day 9 A

--- d-9.py
+++ d-9.py
@@ -2,7 +2,7 @@
 """
 https://adventofcode.com/2017/day/9
 """
-
+from libs.stack import Stack
 import sys
 import re
 
@@ -13,23 +13,48 @@ with open('input/' + infile) as file:
     input = file.read()
 file.close()
 
-def solve():
+lvl_score = []
+lvl_stack = Stack()
+
+def solveA():
     groups = 0
     raw_stream = input
+    # raw_stream = "{{<!!>},{<!!>},{<!!>},{<!!>}}"
 
 
     stream = re.sub(r'(\!.)', "", raw_stream)
-    # print(stream)
     stream = re.sub(r'(<.*?>)', "", stream)
-    
-    print(stream)
+    stream = re.sub(r'(,)', "", stream)
 
+    sa = list(stream)
+    p = 0
+    while p < len(sa):
+        char = sa[p]
+        p += 1
 
-    print("Solution A: ", groups) 
-    print("Solution B: ", groups) 
+        lvl = lvl_stack.size()
+        if char == '{':
+            lvl_stack.push('{')                        
+        if char == '}':
+            lvl_stack.pop()
+            lvl_score.append(lvl)
+
+    result = sum(lvl_score)
+
+    print("Solution A: ", result) 
+
+def solveB():
+    groups = 0
+    raw_stream = input
+    # raw_stream = "{{<!!>},{<!!>},{<!!>},{<!!>}}"
+
+    stream = re.sub(r'(\!.)', "", raw_stream)
+    stream = re.sub(r'(<.*?>)', "", stream)
+
+    print("Solution B: ") 
 
 
 if __name__ == '__main__':
-    solve()
+    solveA()
     print("Finished executing: " + task)
     sys.exit(1)
