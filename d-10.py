bbee998e52c443371de55046be2fb62204b4958f
Peter Steiner <peter.steiner.79.ps@gmail.com>
2017-12-10 08:51:30 +0100

    Prepare day 10

---
+++ d-10.py
@@ -0,0 +1,28 @@
+#!/user/bin/env python3 -tt
+"""
+https://adventofcode.com/2017/day/10
+"""
+
+import sys
+import re
+
+task="d-10"
+infile=task + ".input"
+
+with open('input/' + infile) as file:
+    input = file.read()
+file.close()
+
+def solveA():
+
+    print("Solution A: ") 
+
+def solveB():
+
+    print("Solution B: ") 
+
+if __name__ == '__main__':
+    solveA()
+    # solveB()
+    print("Finished executing: " + task)
+    sys.exit(1)
