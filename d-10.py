711d6be6130c1a6ca2b04f749bb64c69edb691a2
Peter Steiner <peter.steiner.79.ps@gmail.com>
2017-12-10 10:13:30 +0100

    Day 10 A

--- d-10.py
+++ d-10.py
@@ -14,8 +14,38 @@ with open('input/' + infile) as file:
 file.close()
 
 def solveA():
+    # input = "3, 4, 1, 5"
+    lengths = [int(s.rstrip()) for s in input.split(",")]
+    list_size = 256
+    hash_arr = list(range(list_size))
+    position = 0
+    skip = 0
 
-    print("Solution A: ") 
+    for length in lengths:
+        # cut array
+        # pos + length-1
+        # reverse array
+        hash_size = len(hash_arr)
+        # print(hash_arr, length, skip, position)
+        circular_arr = hash_arr + hash_arr
+
+        pos_from = position%hash_size
+        pos_to = pos_from+length
+        sub_list = circular_arr[pos_from:pos_to]
+        reversed_sub_list = sub_list[::-1]
+
+        i = 0
+        while i < len(reversed_sub_list):
+            substitute_pos = (position + i)%hash_size 
+            hash_arr[substitute_pos] = reversed_sub_list[i]
+            i += 1
+        position += (length + skip)%hash_size
+        skip += 1
+
+    result = hash_arr[0]*hash_arr[1]
+
+    # print(hash_arr)
+    print("Solution A: ", result) 
 
 def solveB():
 
