c955e8fa03e6b5e11b691738e56b51c936758339
Peter Steiner <peter.steiner@svt.se>
2017-12-12 07:56:28 +0100

    Day 12 A B

--- d-12.py
+++ d-12.py
@@ -6,25 +6,20 @@ https://adventofcode.com/2017/day/12
 import sys
 import re
 
-task="d-12.1"
+task="d-12"
 infile=task + ".input"
 
 with open('input/' + infile) as file:
     input = file.read()
 file.close()
 
-
-class Pipe():
-    def __init__(self, link_, links_):
-        self.link = link_
-        self.links = links_
-
-    def __str__(self):
-        return "{0} <-> {1}".format(self.link, self.links) 
-
-def printPipes(pipes):
-    for pipe in pipes:
-        print(pipe)
+def visit(prg, pipes, visited, depth):
+    links = pipes.get(prg)
+    if prg in visited:
+        return 
+    visited.append(prg)
+    for link in links:
+        visit(link, pipes, visited, depth + 1)
 
 def solve():
 
@@ -36,39 +31,24 @@ def solve():
     for p in raw_pipes:
         raw_pipe = p.split("<->")
         links = [int(s) for s in raw_pipe[1].split(",")]
-        links.sort()
-        print(links)
-        for l in links:
-            pipe = Pipe(int(raw_pipe[0]), l)
-            pipes.append(pipe)
-            pipe_reverse = Pipe(l, int(raw_pipe[0]))
-            pipes.append(pipe_reverse)
- 
-        # pipe = Pipe(int(raw_pipe[0]), links[0])
-        pipes.append(pipe)
-        # print(pipe)
+        dict_pipes[int(raw_pipe[0])] = links
+        pipes.append(int(raw_pipe[0]))
 
-    pipes.sort(key=lambda p: p.link, reverse=False)
-    # printPipes(pipes)
+    visited = []
+    visit(0, dict_pipes, visited, 0)
 
-    piped_programs = set()
-    piped_programs.add(0)
-    for pipe in pipes:
-        if pipe.link in piped_programs or pipe.links in piped_programs:
-            print("....")
-            piped_programs.add(pipe.link)
-            piped_programs.add(pipe.links)
+    print("A:", len(visited))
 
-        # #  and not pipe.links in links
-        # if pipe.link in links:
-        #     links.append(pipe.links)
+    groups = []
+    for p in pipes:
+        visited = []
+        visit(p, dict_pipes, visited, 0)
+        visited.sort()
+        group = ",".join(str(i) for i in visited)
+        if not group in groups:
+            groups.append(group)
 
-    programs = list(piped_programs)
-    # for link in programs:
-    #     print(link)
-
-    print("A: ", len(programs))
-    # print("B")
+    print("B:", len(groups))
 
 if __name__ == '__main__':
     print("\n")
