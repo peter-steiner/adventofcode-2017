36d65cdac9989cfb8e0e29f1e36b415a97b64f28
Peter Steiner <peter.steiner.79.ps@gmail.com>
2017-12-13 21:11:18 +0100

    Prepare day 14

---
+++ d-14.py
@@ -0,0 +1,30 @@
+#!/user/bin/env python3 -tt
+"""
+https://adventofcode.com/2017/day/14
+"""
+
+import sys
+import re
+
+task="d-14.1"
+infile=task + ".input"
+
+with open('input/' + infile) as file:
+    input = file.read()
+file.close()
+
+def solveB():
+
+    print("B")
+
+def solveA():
+
+    print("A")
+
+if __name__ == '__main__':
+    print("\n")
+    solveA()
+    solveB()
+
+    print("\n************\nFinished: " + task)
+    sys.exit(1)
