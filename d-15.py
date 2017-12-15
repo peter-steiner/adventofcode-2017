76e806d3b14a11fefbe1947cdfaadeb7439b1f51
Peter Steiner <peter.steiner.79.ps@gmail.com>
2017-12-15 11:59:01 +0100

    Day 15 B

--- d-15.py
+++ d-15.py
@@ -4,47 +4,24 @@ https://adventofcode.com/2017/day/15
 """
 
 import sys
-import re
 
 task="d-15"
-infile=task + ".input"
-
 A = 0
 B = 1 
 factor = [16807, 48271]
-gen = [512, 191]
-# gen = [65, 8921]
-
-def solveB():
-    i = 0
-    while True:
-        i += 1
-        l = list("{0:b}".format(i))
-        if len(l) == 17:
-            print(i, l)
-            break
-    print("B")
 
 def solveA():
-    global gen
+    gen = [512, 191]
 
     i = 0
     hits = 0
-    while i < 40000000:
-    # while i < 6:
-        if i%1000000 == 0:
-            print("Progress: ", i)
+    for _ in range(40000000):
 
         gen_a = gen[A] * factor[A] % 2147483647
         gen_b = gen[B] * factor[B] % 2147483647
 
-        bin_list_a = list("{0:b}".format(gen_a))
-        len_a = len(bin_list_a)
-        bin_list_b = list("{0:b}".format(gen_b))
-        len_b = len(bin_list_b)
-
-        seq_a = bin_list_a[::-1]
-        seq_b = bin_list_b[::-1]
+        seq_a = list("{0:b}".format(gen_a))[::-1]
+        seq_b = list("{0:b}".format(gen_b))[::-1]        
         for n in range(16):
             a = seq_a[n] if n < len(seq_a) else 0 
             b = seq_b[n] if n < len(seq_b) else 0 
@@ -53,19 +30,48 @@ def solveA():
             if n == 15:
                 hits += 1
 
-        # print("*****", len(seq_a))
-        # print(seq_a)
-        # print(seq_b)
-
-        i += 1
         gen = [gen_a, gen_b]
 
     print("A", hits)
 
+def solveB():
+    gen = [512, 191]
+    
+    a_sequences = []
+    b_sequences = []
+
+    i = 0
+    while i < 40000000:
+        gen_a = gen[A] * factor[A] % 2147483647
+        gen_b = gen[B] * factor[B] % 2147483647
+        if gen_a%4 == 0:
+            a_sequences.append(gen_a)
+        if gen_b%8 == 0:
+            b_sequences.append(gen_b)
+        i += 1
+        gen = [gen_a, gen_b]
+
+    hits = 0
+    i = 0
+    max_iterations = len(b_sequences) if len(b_sequences) < len(a_sequences) else len(a_sequences)
+    while i < max_iterations:
+        seq_a = list("{0:b}".format(a_sequences[i]))[::-1]
+        seq_b = list("{0:b}".format(b_sequences[i]))[::-1]
+        for n in range(16):
+            a = seq_a[n] if n < len(seq_a) else 0 
+            b = seq_b[n] if n < len(seq_b) else 0 
+            if a != b:
+                break
+            if n == 15:
+                hits += 1
+        i += 1
+    print("B", hits)
+
+
 if __name__ == '__main__':
     print("\n")
     solveA()
-    # solveB()
+    solveB()
 
     print("\n************\nFinished: " + task)
     sys.exit(1)
---
+++ d-16.py
@@ -0,0 +1,27 @@
+#!/user/bin/env python3 -tt
+"""
+https://adventofcode.com/2017/day/16
+"""
+
+import sys
+
+task="d-16"
+infile=task + ".input"
+
+
+
+def solveA():
+
+    print("A:")
+    
+def solveB():
+
+    print("B:")
+
+if __name__ == '__main__':
+    print("\n")
+    solveA()
+    solveB()
+
+    print("\n************\nFinished: " + task)
+    sys.exit(1)
