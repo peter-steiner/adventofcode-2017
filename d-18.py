03328d62c72488e3d25e9169c7481b8837c808de
Peter Steiner <peter.steiner.79.ps@gmail.com>
2017-12-18 00:05:34 +0100

    Prepare day 18

---
+++ d-18.py
@@ -0,0 +1,25 @@
+#!/user/bin/env python3 -tt
+"""
+https://adventofcode.com/2017/day/18
+"""
+
+import sys
+import re
+
+task="d-18"
+infile=task + ".input"
+
+def solveA():
+
+    print("A:")
+
+def solveB():
+    print("B:")
+
+if __name__ == '__main__':
+    print("\n")
+    solveA()
+    solveB()
+
+    print("\n************\nFinished: " + task)
+    sys.exit(1)
