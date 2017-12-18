3e2fbbda0166ceadb7f525b9103caa23ac3fd896
Peter Steiner <peter.steiner.79.ps@gmail.com>
2017-12-18 23:19:24 +0100

    Prepare Day 19

---
+++ d-19.py
@@ -0,0 +1,26 @@
+#!/user/bin/env python3 -tt
+"""
+https://adventofcode.com/2VALUE17/day/19
+"""
+
+import sys
+import re
+
+task="d-19.1"
+infile=task + ".input"
+
+with open('input/' + infile) as file:
+    input = file.read()
+file.close()
+
+
+def solveA():
+
+    print("Bring it on...")
+
+if __name__ == '__main__':
+    print("\n")
+    solveA()
+
+    print("\n************\nFinished: " + task)
+    sys.exit(1)
